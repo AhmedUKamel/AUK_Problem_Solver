e827aee682bf3060165098050b865fc16f237a1e
AhmedUKamel <ahmedukamel@outlook.com>
2024-01-21 19:53:46 +0200

    Update views

--- src/main/java/com/ahmedukamel/problemsolver/service/UserService.java
+++ src/main/java/com/ahmedukamel/problemsolver/service/UserService.java
@@ -1,5 +1,12 @@
 package com.ahmedukamel.problemsolver.service;
 
+import com.ahmedukamel.problemsolver.dto.UpdateUserRequest;
+import com.ahmedukamel.problemsolver.model.User;
+
 public interface UserService {
     boolean isEmailExists(String email);
+
+    void updatePassword(User user, String newPassword);
+
+    void updateProfile(User user, UpdateUserRequest request);
 }
