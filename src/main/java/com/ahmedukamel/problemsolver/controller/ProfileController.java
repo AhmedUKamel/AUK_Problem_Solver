a74acea10c6b6543266cc3eccebaece609ca5433
AhmedUKamel <ahmedukamel@outlook.com>
2024-01-21 19:55:13 +0200

    Update ProfileController

--- src/main/java/com/ahmedukamel/problemsolver/controller/ProfileController.java
+++ src/main/java/com/ahmedukamel/problemsolver/controller/ProfileController.java
@@ -3,67 +3,62 @@ package com.ahmedukamel.problemsolver.controller;
 import com.ahmedukamel.problemsolver.dto.UpdatePasswordRequest;
 import com.ahmedukamel.problemsolver.dto.UpdateUserRequest;
 import com.ahmedukamel.problemsolver.model.User;
-import com.ahmedukamel.problemsolver.service.ProfileService;
-import com.ahmedukamel.problemsolver.util.AuthenticatedUser;
-import jakarta.servlet.http.HttpServletRequest;
+import com.ahmedukamel.problemsolver.service.UserService;
+import com.ahmedukamel.problemsolver.util.MenuUtils;
+import com.ahmedukamel.problemsolver.util.ModalUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("user/profile")
+@PreAuthorize("isAuthenticated()")
 public class ProfileController {
-    private final ProfileService profileService;
+    private final UserService userService;
 
     @GetMapping
-    public String updateProfilePage(Model model) {
-        User user = AuthenticatedUser.getAuthenticatedUser();
-        if (user != null) {
-            model.addAttribute("user", user);
-            model.addAttribute("menuProfile", true);
-            model.addAttribute("profilePersonalInformation", true);
-            model.addAttribute("request", UpdateUserRequest.builder().name(user.getName()).title(user.getTitle()).bio(user.getBio()).picture(user.getPicture()).build());
-        }
-        return "user-personal-information";
+    public ModelAndView getProfilePage(@AuthenticationPrincipal User user) {
+        ModelAndView modelAndView = new ModelAndView("user-personal-information", Map.of("user", user));
+        modelAndView.addObject("request", new UpdateUserRequest(user));
+        MenuUtils.profilePage(modelAndView);
+        return modelAndView;
     }
 
     @PostMapping
-    public String updateProfile(@Validated @ModelAttribute("request") UpdateUserRequest request, BindingResult result, Model model) {
+    public ModelAndView updateProfile(@Validated @ModelAttribute("request") UpdateUserRequest request, BindingResult result, @AuthenticationPrincipal User user) {
+        ModelAndView modelAndView = new ModelAndView("user-personal-information", Map.of("user", user));
+        MenuUtils.profilePage(modelAndView);
         if (!result.hasErrors()) {
-            profileService.updateProfile(request);
-            model.addAttribute("updatedSuccessfully", true);
+            userService.updateProfile(user, request);
+            ModalUtils.successfulUpdateProfile(modelAndView);
         }
-        model.addAttribute("user", AuthenticatedUser.getAuthenticatedUser());
-        model.addAttribute("menuProfile", true);
-        model.addAttribute("profilePersonalInformation", true);
-        return "user-personal-information";
+        return modelAndView;
     }
 
     @GetMapping("password/update")
-    public String updatePasswordPage(Model model) {
-        User authenticatedUser = AuthenticatedUser.getAuthenticatedUser();
-        if (authenticatedUser != null) {
-            model.addAttribute("user", authenticatedUser);
-            model.addAttribute("menuProfile", true);
-            model.addAttribute("profileChangePassword", true);
-            model.addAttribute("request", UpdatePasswordRequest.builder().email(authenticatedUser.getEmail()).build());
-        }
-        return "user-change-password";
+    public ModelAndView updatePasswordPage(@AuthenticationPrincipal User user) {
+        ModelAndView modelAndView = new ModelAndView("user-change-password", Map.of("user", user));
+        modelAndView.addObject("request", new UpdatePasswordRequest(user));
+        MenuUtils.changePasswordPage(modelAndView);
+        return modelAndView;
     }
 
     @PostMapping("password/update")
-    public String updatePassword(@Validated @ModelAttribute("request") UpdatePasswordRequest request, BindingResult result, HttpServletRequest httpServletRequest, Model model) {
-        model.addAttribute("user", AuthenticatedUser.getAuthenticatedUser());
-        model.addAttribute("menuProfile", true);
-        model.addAttribute("profileChangePassword", true);
+    public ModelAndView updatePassword(@Validated @ModelAttribute("request") UpdatePasswordRequest request, BindingResult result, @AuthenticationPrincipal User user) {
+        ModelAndView modelAndView = new ModelAndView("user-change-password", Map.of("user", user));
+        MenuUtils.changePasswordPage(modelAndView);
         if (!result.hasErrors()) {
-            profileService.updatePassword(httpServletRequest, request.getNewPassword());
-            model.addAttribute("updatedSuccessfully", true);
+            userService.updatePassword(user, request.getNewPassword());
+            ModalUtils.successfulUpdateProfile(modelAndView);
         }
-        return "user-change-password";
+        return modelAndView;
     }
 }
